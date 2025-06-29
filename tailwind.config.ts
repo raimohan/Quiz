9e2851a01bb26cc2b03abda78c0d12880ad4cd75
raimohan <raimohan9676@gmail.com>
2025-06-29 18:09:37 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        sans: ['Lato', 'sans-serif'],
+        anton: ['Anton', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
